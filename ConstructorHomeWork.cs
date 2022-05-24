9437411ebbbaa088fb5cbcca6f9085459dab097d
Mirsaleh <mirsalehaa@code.edu.az>
2022-05-24 18:10:08 +0400

    Constructor

---
+++ ConstructorHomeWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework999
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Murad", "Agayev", 22);
+            
+
+
+        }
+    }
+
+}
---
+++ studentWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework999
+{
+     class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAbult;
+        public Person(string name, string surname, int age )
+        {
+            IsAbult = false;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            if (Age > 18)
+            {
+                IsAbult |= true;
+                Console.WriteLine(IsAbult);
+            }
+            else 
+            {
+                Console.WriteLine(IsAbult);
+            }
+        }
+    }
+
+   
+}
